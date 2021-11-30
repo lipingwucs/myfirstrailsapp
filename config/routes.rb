d765b7972ade289417fa88bda3ce45a39bc67aae
Liping Wu <lipingwucs@gmail.com>
2021-11-30 10:02:08 -0500

    customize Home page MVC to replace the default MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "/", to: "home#index"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'blog', to: 'blog#index'  # customize blog route
+
+  # Get /about
+  get "about", to: "about#index"
 end
