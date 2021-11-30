ddb99394b020959f6745610e9e3f1aa335a3ca41
Liping Wu <lipingwucs@gmail.com>
2021-11-30 10:28:56 -0500

    add bootsrap css and js bundle

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  get "/", to: "home#index"
+  # get "/", to: "home#index"
+  root to: "home#index"         # another way to route URL 
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'blog', to: 'blog#index'  # customize blog route
 
   # Get /about
